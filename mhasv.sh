e2b9cb06c20198dbc6cb6385cf8ea0c315793c5f
LeoLeoLeo <lra21711214@yahoo.co.jp>
2018-12-20 22:04:45 +0900

    Commit

---
+++ mhasv.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+mkdir Workspace
+
+cd Workspace
+
+curl -o index.html https://raw.githubusercontent.com/lra21711214/file/master/index.html
+
+mkdir css
+
+touch css/stylesheet.css
+
+mkdir javascript
+
+touch javascript/main.js
+
+vi index.html
+
+vi css/stylesheet.css
